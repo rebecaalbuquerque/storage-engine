d41777548aeeb4b2da800a79c5a89cf68d99ffc5
Rebeca <rebeca.albuquerque96@gmail.com>
2019-02-17 15:48:21 -0300

    Realocando arquivo gitignore

--- src/main/Teste.java
+++ src/main/Teste.java
@@ -1,5 +1,6 @@
 package main;
 
+import model.BlocoControle;
 import model.BlocoDado;
 
 import java.util.Arrays;
@@ -7,17 +8,25 @@ import java.util.Arrays;
 public class Teste {
 
     public static void main(String[] args) {
+        System.out.println("Testando bloco de controle");
+        testarBlocoControle();
 
-        /*String[] array = new String[]{ "COLA[I(6)]", "COLB[A(25)]" };
-        BlocoControle bloco = new BlocoControle(0, array);
-        System.out.println(bloco.getInformacoesCompletas().length);
-        System.out.println(Arrays.toString(bloco.getInformacoesCompletas()));*/
+        System.out.println("Testando bloco de dados");
+        testarBlocoDados();
+    }
 
+   private static void testarBlocoDados(){
         String[] array = new String[]{ "6545", "abcdef" };
         BlocoDado bloco = new BlocoDado(0, array);
         System.out.println(bloco.getInformacoesCompletas().length);
         System.out.println(Arrays.toString(bloco.getInformacoesCompletas()));
-    }
+   }
 
+    private static void testarBlocoControle(){
+        String[] array = new String[]{ "COLA[I(6)]", "COLB[A(25)]" };
+        BlocoControle bloco = new BlocoControle(0, array);
+        System.out.println(bloco.getInformacoesCompletas().length);
+        System.out.println(Arrays.toString(bloco.getInformacoesCompletas()));
+    }
 
 }
